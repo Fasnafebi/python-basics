646f5950a68b2c4f64d4e24894677ae6d0468323
Fasnafebi <fasnafebi3@gmail.com>
2025-07-31 22:34:02 +0530

    Add files via upload

---
+++ day01/foundationtask.py
@@ -0,0 +1,5 @@
+favorite_foods = ["pizza", "chocolate", "sushi", "ice cream", "Pasta"]
+
+print("My favorite foods:")
+for i, food in enumerate(favorite_foods, start=1):
+    print(f"\t{i}\t{food}")
